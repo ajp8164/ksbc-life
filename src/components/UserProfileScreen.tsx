9ceb73791de7efe869cdc26ae1fe11a026aa5a29
Andy Phillipson <ajp@mindspring.com>
2023-02-28 15:54:01 -0500

    Add avatar to profile screen

--- src/components/UserProfileScreen.tsx
+++ src/components/UserProfileScreen.tsx
@@ -3,15 +3,17 @@ import {
   MoreNavigatorParamList,
   MainNavigatorParamList,
 } from 'types/navigation';
-import { ScrollView, View } from 'react-native';
+import { Alert, ScrollView, View } from 'react-native';
 import React, { useEffect } from 'react';
 import { makeStyles } from '@rneui/themed';
 import { CompositeScreenProps } from '@react-navigation/core';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { useSelector } from 'react-redux';
 import { selectUser } from 'store/selectors/userProfileSelectors';
-import { Button } from '@rneui/base';
+import { Icon, Image } from '@rneui/base';
 import { signOut } from 'lib/userAuthentication';
+import { Divider, ListItem } from '@react-native-ajp-elements/ui';
+import { biometricAuthentication } from 'lib/biometricAuthentication';
 
 type Props = CompositeScreenProps<
   NativeStackScreenProps<MoreNavigatorParamList, 'UserProfile'>,
@@ -30,6 +32,29 @@ const UserProfileScreen = ({ navigation }: Props) => {
     });
   });
 
+  const confirmSignOut = async () => {
+    await biometricAuthentication()
+      .then(() => {
+        Alert.alert(
+          'Confirm Signing Out',
+          'Are you sure you want to signout?',
+          [
+            {
+              text: 'Yes, sign out',
+              onPress: doSignOut,
+              style: 'destructive',
+            },
+            {
+              text: 'No',
+              style: 'cancel',
+            },
+          ],
+          { cancelable: false },
+        );
+      })
+      .catch();
+  };
+
   const doSignOut = () => {
     signOut()
       .then(() => {
@@ -47,23 +72,47 @@ const UserProfileScreen = ({ navigation }: Props) => {
         style={theme.styles.view}
         showsVerticalScrollIndicator={false}
         contentInsetAdjustmentBehavior={'automatic'}>
-        <Button
-          title={'SIGN OUT'}
-          titleStyle={theme.styles.buttonClearTitle}
-          buttonStyle={theme.styles.buttonClear}
-          containerStyle={s.signInButtonContainer}
-          onPress={doSignOut}
+        {user?.photoURL ? (
+          <Image source={{ uri: user?.photoURL }} containerStyle={s.avatar} />
+        ) : (
+          <Icon
+            name="account-circle"
+            type={'material-community'}
+            color={theme.colors.brandSecondary}
+            size={28}
+          />
+        )}
+        <Divider />
+        <ListItem
+          title={'Sign Out'}
+          titleStyle={s.signOut}
+          position={['first', 'last']}
+          rightImage={false}
+          onPress={confirmSignOut}
         />
       </ScrollView>
     </View>
   );
 };
 
-const useStyles = makeStyles((_theme, __theme: AppTheme) => ({
+const useStyles = makeStyles((_theme, theme: AppTheme) => ({
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    alignSelf: 'center',
+    marginTop: 15,
+  },
   signInButtonContainer: {
     width: '80%',
     alignSelf: 'center',
     marginBottom: 15,
+  },
+  signOut: {
+    ...theme.styles.textBold,
+    textAlign: 'center',
+    width: '100%',
+    color: theme.colors.brandPrimary,
   },
 }));
 
