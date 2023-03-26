149c7a62c92b55e271f223511bd26389ab411608
Andy Phillipson <ajp@mindspring.com>
2023-03-25 23:47:26 -0400

    Theme for switch

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -12,9 +12,20 @@ export const theme = createTheme(
       cardBackground: '#202020',
       listItemBackgroundAlt: '#101010',
       shadowColor: '#00000000',
+
       ...Platform.select({
         ios: {
           screenHeaderBackButton: '#80BFE1',
+          switchOffThumb: '#ffffff',
+          switchOnThumb: '#ffffff',
+          switchOffTrack: '#e5e5e5',
+          switchOnTrack: '#80BFE1',
+        },
+        android: {
+          switchOffThumb: '#787878',
+          switchOnThumb: '#80BFE1',
+          switchOffTrack: '#e5e5e5',
+          switchOnTrack: '#80BFE140',
         },
       }),
     },
@@ -25,9 +36,22 @@ export const theme = createTheme(
       cardBackground: '#FFFFFF',
       listItemBackgroundAlt: '#f7f7f7',
       shadowColor: '#000000',
+
       ...Platform.select({
         ios: {
           screenHeaderBackButton: '#80BFE1',
+          switchOffThumb: '#ffffff',
+          switchOnThumb: '#ffffff',
+          switchOffTrack: '#787878',
+          switchOnTrack: '#80BFE1',
+        },
+        android: {
+          switchOffThumb: '#cccccc',
+          switchOnThumb: '#80BFE1',
+          switchOffTrack: '#787878',
+          switchOnTrack: '#80BFE140',
+          screenHeaderBackButton: '#000000',
+          screenHeaderInvBackButton: '#ffffff',
         },
       }),
     },
