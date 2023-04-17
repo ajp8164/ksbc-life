82d9a04136d09ddf166211d0d4909cfa96ca31e3
Andy Phillipson <ajp@mindspring.com>
2023-04-17 10:46:33 -0400

    Change user caching to a collection listener

--- src/firebase/firestore/cache/cacheUsers.ts
+++ src/firebase/firestore/cache/cacheUsers.ts
@@ -1,10 +1,16 @@
+import { UserProfile } from 'types/user';
 import { cacheUserProfiles } from 'store/slices/cache';
 import { dispatch } from 'store';
-import { getUsers } from 'firebase/firestore/users';
 import { log } from '@react-native-ajp-elements/core';
+import { usersCollectionChangeListener } from 'firebase/firestore/users';
 
 export const cacheUsers = async () => {
-  const users = await getUsers();
-  dispatch(cacheUserProfiles({ userProfiles: users.result }));
-  log.debug(`Cached ${users.result.length} users from firestore`);
+  usersCollectionChangeListener(snapshot => {
+    const users: UserProfile[] = [];
+    snapshot.docs.forEach(d => {
+      users.push({ ...d.data(), id: d.id } as UserProfile);
+    });
+    dispatch(cacheUserProfiles({ userProfiles: users }));
+    log.debug(`Cached ${users.length} users from firestore`);
+  });
 };
