08a9145f89d4f615e3d191820adbd3ce8ad305d6
Andy Phillipson <ajp@mindspring.com>
2023-07-12 14:27:07 -0400

    Add loading to NoItem component

--- src/components/atoms/NoItems.tsx
+++ src/components/atoms/NoItems.tsx
@@ -1,25 +1,30 @@
+import { ActivityIndicator, Text, View } from 'react-native';
 import { AppTheme, useTheme } from 'theme';
-import { Text, View } from 'react-native';
 
 import { Icon } from '@rneui/base';
 import { makeStyles } from '@rneui/themed';
 
 interface NoItemsInterface {
+  isLoading?: boolean;
   title?: string;
 }
 
-const NoItems = ({ title }: NoItemsInterface) => {
+const NoItems = ({ isLoading, title }: NoItemsInterface) => {
   const theme = useTheme();
   const s = useStyles(theme);
 
   return (
     <View style={[s.noItems]}>
-      <Icon
-        name={'emoticon-sad-outline'}
-        type={'material-community'}
-        color={theme.colors.brandSecondary}
-        size={48}
-      />
+      {isLoading ? (
+        <ActivityIndicator size={'large'} color={theme.colors.brandPrimary} />
+      ) : (
+        <Icon
+          name={'emoticon-sad-outline'}
+          type={'material-community'}
+          color={theme.colors.brandSecondary}
+          size={48}
+        />
+      )}
       <Text style={theme.styles.textNormal}>{title}</Text>
     </View>
   );
